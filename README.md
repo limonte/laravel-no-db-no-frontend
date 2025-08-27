f593471c6d4211cc06ecb0daaf23dcab6e04269b
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-08-27 14:23:39 +0300

    chore: init the simplest possible Laravel app

---
+++ README.md
@@ -0,0 +1,7 @@
+Very simple Laravel app w/o front-end and DB
+
+```sh
+php artisan serve
+```
+
+to start it
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    </head>
+    <body>
+        <p>This is a very simple Laravel application without front-end and database.</p>
+    </body>
+</html>
